9d4bad84dd7ffb1c1cb92e70715c93c5d15ed7f5
AwesomeAim <30537683+AwesomeAim@users.noreply.github.com>
2022-01-12 04:02:22 +0000

    no head pls

--- server/commands/fun.py
+++ server/commands/fun.py
@@ -93,7 +93,6 @@ def ooc_cmd_unshake(client, arg):
     """
     if len(arg) == 0:
         raise ArgumentError("You must specify a target.")
-<<<<<<< HEAD
     try:
         targets = client.server.client_manager.get_targets(
             client, TargetType.ID, int(arg), False
@@ -117,22 +116,17 @@ def ooc_cmd_typo(client, arg):
     """
     if len(arg) == 0:
         raise ArgumentError('You must specify a target.')
-=======
->>>>>>> 7356e7886476b70d2b92e8c51c15c88efee3c898
     try:
         targets = client.server.client_manager.get_targets(
             client, TargetType.ID, int(arg), False
         )
     except:
-<<<<<<< HEAD
-        raise ArgumentError('You must specify a target. Use /typo <id>.')
+        raise ArgumentError("You must specify a target. Use /unshake <id>.")
     if targets:
         for c in targets:
-            database.log_area('typo', client, client.area, target=c)
-            c.typo = True
-        client.send_ooc(f'Typo\'d {len(targets)} existing client(s).')
-    else:
-        client.send_ooc('No targets found.')
+            database.log_area("unshake", client, client.area, target=c)
+            c.shaken = False
+        client.send_ooc(f"Unshook {len(targets)} existing client(s).")
 
 
 @mod_only()
@@ -196,13 +190,5 @@ def ooc_cmd_unformal(client, arg):
             database.log_area('unformal', client, client.area, target=c)
             c.formal = False
         client.send_ooc(f'Allowed {len(targets)} existing client(s) to be not formal.')
-=======
-        raise ArgumentError("You must specify a target. Use /unshake <id>.")
-    if targets:
-        for c in targets:
-            database.log_area("unshake", client, client.area, target=c)
-            c.shaken = False
-        client.send_ooc(f"Unshook {len(targets)} existing client(s).")
->>>>>>> 7356e7886476b70d2b92e8c51c15c88efee3c898
     else:
         client.send_ooc("No targets found.")
